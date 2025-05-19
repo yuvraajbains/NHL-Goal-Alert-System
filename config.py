aeb4d05d174aad5e021a423e59380f9a65db53cf
Yuvraj Bains <168843781+yuvraajbains@users.noreply.github.com>
2025-05-18 23:18:58 -0400

    Initialize

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID")
 AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
 TWILIO_PHONE = os.getenv("TWILIO_PHONE")
 MY_PHONE = os.getenv("MY_PHONE")
-WATCH_TEAMS = ["Oilers", "Leafs"]  # Can expand to be dynamic if needed
+WATCH_TEAMS = ["Oilers"]  # Can expand to be dynamic if needed
 
 required = [ACCOUNT_SID, AUTH_TOKEN, TWILIO_PHONE, MY_PHONE]
 if not all(required):
